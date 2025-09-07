b8f355e5070af3357b78a4284f11ecc3a38a2d86
gandan <gandan.dev@gmail.com>
2025-09-07 10:51:33 +0900

    chore: unplugin icons 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import tailwindcss from '@tailwindcss/vite'
 import { sveltekit } from '@sveltejs/kit/vite'
+import Icons from 'unplugin-icons/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()]
+  plugins: [tailwindcss(), sveltekit(), Icons({
+    compiler: 'svelte'
+  })]
 })
